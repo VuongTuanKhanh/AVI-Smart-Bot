57eef811bd8cdbd1734d9803df53745dd69812db
Archius Vương <khanhvuongtuan37@gmail.com>
2022-08-09 23:43:24 +0700

    Add pip install function

--- core/helper.py
+++ core/helper.py
@@ -1,25 +1,13 @@
-def get_logger(name='logger', file='', stream_fmt='%(name)s - %(levelname)s - %(message)s', file_fmt='%(asctime)s - %(name)s - %(levelname)s - %(message)s'):
-    import logging
+def pip_install(module_name):
+    """Pip install module
 
-    logger = logging.getLogger(name)
+    Args:
+        module_name (__str__): Name of the module to install
+    """
+    import sys
+    import subprocess
 
-    # Create handlers
-    stream_handler = logging.StreamHandler()
-    if file:
-        file_handler = logging.FileHandler(file)
-
-    # Configure level and formatter and add it to handlers
-    stream_handler.setLevel(logging.INFO) # warning and above is logged to the stream
-    file_handler.setLevel(logging.INFO) # error and above is logged to a file
-
-    stream_format = logging.Formatter(stream_fmt)
-    file_format = logging.Formatter(file_fmt)
-
-    stream_handler.setFormatter(stream_format)
-    file_handler.setFormatter(file_format)
-
-    # Add handlers to the logger
-    logger.addHandler(stream_handler)
-    logger.addHandler(file_handler)
-
-    return logger
+    print(f"Module '{module_name}' is not installed ! \n Installing ...")
+    subprocess.check_call(
+        [sys.executable, '-m', 'pip', 'install', module_name])
+    print("Finish installation !!!")
