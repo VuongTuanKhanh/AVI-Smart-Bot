bfcf8d711c0b85e0c5ed9157957d31f132884b08
Archius Vương <khanhvuongtuan37@gmail.com>
2022-08-09 23:51:56 +0700

    Add run shell

--- core/helper.py
+++ core/helper.py
@@ -11,3 +11,13 @@ def pip_install(module_name):
     subprocess.check_call(
         [sys.executable, '-m', 'pip', 'install', module_name])
     print("Finish installation !!!")
+
+
+def run_shell(cmd):
+    """Execute a Shell command on Python
+    Args:
+        cmd (str): A specific Shell command that need to be execute in the Python enviroment
+    """
+    import subprocess
+    # Create a sub process
+    proc = subprocess.Popen(cmd, shell=True, stdin=None, stdout=None, stderr=None, executable="/bin/bash")
