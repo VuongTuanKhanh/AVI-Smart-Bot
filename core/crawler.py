29c658dc4d47a5b24f44273904cb152bba6830ee
Archius Vương <khanhvuongtuan37@gmail.com>
2022-08-10 13:30:25 +0700

    add exception handler for data posts

--- core/crawler.py
+++ core/crawler.py
@@ -114,14 +114,18 @@ class Crawler():
             let feeds = document.getElementsByClassName("du4w35lb k4urcfbm l9j0dhe7 sjgh65i0")
             for (let feed of feeds)
             {
-                let id = feed.querySelector('a[class="oajrlxb2 g5ia77u1 qu0x051f esr5mh6w e9989ue4 r7d6kgcz rq0escxv nhd2j8a9 nc684nl6 p7hjln8o kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x jb3vyjys rz4wbd8a qt6c0cv9 a8nywdso i1ao9s8h esuyzwwr f1sip0of lzcic4wl gmql0nx0 gpro0wi8 b1v8xokw"').getAttribute('href').split('/')[6];
+                let id = feed.querySelector('a[class="oajrlxb2 g5ia77u1 qu0x051f esr5mh6w e9989ue4 r7d6kgcz rq0escxv nhd2j8a9 nc684nl6 p7hjln8o kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x jb3vyjys rz4wbd8a qt6c0cv9 a8nywdso i1ao9s8h esuyzwwr f1sip0of lzcic4wl gmql0nx0 gpro0wi8 b1v8xokw"').getAttribute('href').split('/')[6]
                 
                 let answers = []
                 
                 feed_question = feed.querySelector('div[class="dati1w0a ihqw7lf3 hv4rvrfc ecm0bbzt"]')
                 feed_answers = feed.querySelectorAll('div[class="ecm0bbzt e5nlhep0 a8c37x1j"]')
-
-                question = feed_question.innerText
+                try {
+                    question = feed_question.innerText
+                }
+                catch { 
+                    continue
+                }
                 for (let answer of feed_answers) {
                     data = answer.innerText
                     answers.push(data)
