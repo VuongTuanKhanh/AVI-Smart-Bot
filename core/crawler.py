ece73fcd8deb1721e8865771f4696dacbb8fd5f7
Archius Vương <khanhvuongtuan37@gmail.com>
2022-08-10 12:55:37 +0700

    Add post id

--- core/crawler.py
+++ core/crawler.py
@@ -13,17 +13,15 @@ class Crawler():
             copy_from (str, optional): Original Path. Defaults to '/usr/lib/chromium-browser/chromedriver'.
             copy_to (str, optional): Destination_path. Defaults to '/usr/bin'.
         """
-        
+
         from time import sleep
 
         pip_install('selenium')
         run_shell('apt-get update')
-        
-        sleep(10)
-        
-        apt_install('chromium-chromedriver')
-        
 
+        sleep(10)
+
+        apt_install('chromium-chromedriver')
 
     def get_browser(self):
         """Open a browser and go to the url
@@ -42,7 +40,6 @@ class Crawler():
         self.browser = webdriver.Chrome(
             '../../usr/bin/chromedriver', options=chrome_options)
 
-
     def get_url(self, url=''):
         """Open a browser and go to the url
         Args:
@@ -52,29 +49,31 @@ class Crawler():
         if url:
             self.url = url
         self.browser.get(url)
-        
-        
+
     def login_workplace(self, username='', password=''):
         """Login to the website
         Args:
             username (str): Username of the user
             password (str): Password of the user
         """
-        
+
         if not username:
             username = self.username
-            
+
         if not password:
             password = self.password
 
         if self.url:
             from selenium.webdriver.common.by import By
-            
-            self.browser.find_element(By.ID, "login_password_submit_button").click()
+
+            self.browser.find_element(
+                By.ID, "login_password_submit_button").click()
             sleep(self.wait_time)
-            self.browser.find_element(By.CSS_SELECTOR, '[name="email"]').send_keys(username)
+            self.browser.find_element(
+                By.CSS_SELECTOR, '[name="email"]').send_keys(username)
             sleep(self.wait_time)
-            self.browser.find_element(By.CSS_SELECTOR, '[type="submit"]').click()
+            self.browser.find_element(
+                By.CSS_SELECTOR, '[type="submit"]').click()
             sleep(2 * self.wait_time)
             self.browser.find_element(By.ID, "i0116").send_keys(self.username)
             sleep(self.wait_time)
@@ -86,14 +85,13 @@ class Crawler():
             sleep(self.wait_time)
             self.browser.find_element(By.ID, "idSIButton9").click()
             sleep(self.wait_time)
-    
-    
+
     def scroll(self, times=0, wait=0):
         for i in range(times):
-            self.browser.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+            self.browser.execute_script(
+                'window.scrollTo(0, document.body.scrollHeight);')
             sleep(wait)
-            
-            
+
     def see_more(self):
         """Click on the 'See more' button
         """
@@ -105,8 +103,7 @@ class Crawler():
                 await new Promise(r => setTimeout(r, 1000));
             }
         ''')
-        
-        
+
     def get_text(self):
         """Get the text of the current page
         """
@@ -117,6 +114,8 @@ class Crawler():
             let feeds = document.getElementsByClassName("du4w35lb k4urcfbm l9j0dhe7 sjgh65i0")
             for (let feed of feeds)
             {
+                let id = feed.querySelector('a[class="oajrlxb2 g5ia77u1 qu0x051f esr5mh6w e9989ue4 r7d6kgcz rq0escxv nhd2j8a9 nc684nl6 p7hjln8o kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x jb3vyjys rz4wbd8a qt6c0cv9 a8nywdso i1ao9s8h esuyzwwr f1sip0of lzcic4wl gmql0nx0 gpro0wi8 b1v8xokw"').getAttribute('href').split('/')[6];
+                
                 let answers = []
                 
                 feed_question = feed.querySelector('div[class="dati1w0a ihqw7lf3 hv4rvrfc ecm0bbzt"]')
@@ -128,69 +127,60 @@ class Crawler():
                     answers.push(data)
                 }
                 results.push({
+                    id: id,
                     question: question,
                     answers: answers
                 })
             }
             return results
         ''')
-        
-    
+
     def crawl_data(self):
         self.see_more()
         self.data = self.get_text()
-        
-    
+
     def close(self):
         """Close the browser
         """
 
         self.browser.close()
-        
-        
+
     @property
     def username(self):
         return 'khanhvt3@fsoft.com.vn'
-    
-    
+
     @property
     def password(self):
         return 'LupusReginaAIEngineer16/11/1999'
-    
-    
+
     @property
     def wait_time(self):
         return 2
-        
-        
+
     def get_source(self):
         """Get the source of the current page
         """
 
         return self.browser.page_source
-    
-    
+
     def get_title(self):
         """Get the title of the current page
         """
 
         return self.browser.title
-    
-    
+
     def get_urls(self):
         """Get the urls of the current page
         """
 
         return self.browser.find_elements_by_tag_name('a')
-    
-    
+
     def get_urls_text(self):
         """Get the urls of the current page
         """
 
         return [url.text for url in self.get_urls()]
-    
-    
+
     def get_urls_href(self):
         """Get the urls of the current page
         """
